a47290ff3a924502c5f239610f1b9a9f1a0f702c
Marte De Leeuw <martedeleeuw@hotmail.com>
2020-12-15 18:53:41 +0100

    Seconde part of Day3

--- src/day3/index.js
+++ src/day3/index.js
@@ -26,11 +26,119 @@ const goA = (input) => {
       countTreesArray.push(1);
     }
   }
-  console.log(countTreesArray.length);
+  return countTreesArray.length;
 };
 
 const goB = (input) => {
-  return;
+  const inputArray = input.split("\r\n");
+  // console.log(inputArray);
+
+  const countTreesArraySlope1 = [];
+  const countTreesArraySlope2 = [];
+  const countTreesArraySlope3 = [];
+  const countTreesArraySlope4 = [];
+  const countTreesArraySlope5 = [];
+
+  let x = 0;
+
+  for (let y = 1; y < inputArray.length; y++) {
+    const treeLineString = inputArray[y];
+    // console.log(treeLineString);
+
+    const treeLineArray = treeLineString.split("");
+    // console.log(treeLineArray);
+
+    x++;
+
+    // console.log(treeLineArray[x]);
+
+    if (treeLineArray[x % treeLineArray.length] == "#") {
+      countTreesArraySlope1.push(1);
+    }
+  }
+
+  x = 0;
+
+  for (let y = 1; y < inputArray.length; y++) {
+    const treeLineString = inputArray[y];
+    // console.log(treeLineString);
+
+    const treeLineArray = treeLineString.split("");
+    // console.log(treeLineArray);
+
+    x += 3;
+
+    // console.log(treeLineArray[x]);
+
+    if (treeLineArray[x % treeLineArray.length] == "#") {
+      countTreesArraySlope2.push(1);
+    }
+  }
+
+  x = 0;
+
+  for (let y = 1; y < inputArray.length; y++) {
+    const treeLineString = inputArray[y];
+    // console.log(treeLineString);
+
+    const treeLineArray = treeLineString.split("");
+    // console.log(treeLineArray);
+
+    x += 5;
+
+    // console.log(treeLineArray[x]);
+
+    if (treeLineArray[x % treeLineArray.length] == "#") {
+      countTreesArraySlope3.push(1);
+    }
+  }
+
+  x = 0;
+
+  for (let y = 1; y < inputArray.length; y++) {
+    const treeLineString = inputArray[y];
+    // console.log(treeLineString);
+
+    const treeLineArray = treeLineString.split("");
+    // console.log(treeLineArray);
+
+    x += 7;
+
+    // console.log(treeLineArray[x]);
+
+    if (treeLineArray[x % treeLineArray.length] == "#") {
+      countTreesArraySlope4.push(1);
+    }
+  }
+
+  x = 0;
+
+  for (let y = 2; y < inputArray.length; y += 2) {
+    const treeLineString = inputArray[y];
+    // console.log(treeLineString);
+
+    const treeLineArray = treeLineString.split("");
+    // console.log(treeLineArray);
+
+    x++;
+
+    // console.log(treeLineArray[x]);
+
+    if (treeLineArray[x % treeLineArray.length] == "#") {
+      countTreesArraySlope5.push(1);
+    }
+  }
+  
+  /* console.log(countTreesArraySlope1.length);
+  console.log(countTreesArraySlope2.length);
+  console.log(countTreesArraySlope3.length);
+  console.log(countTreesArraySlope4.length);
+  console.log(countTreesArraySlope5.length); */
+
+  const productOfTrees = countTreesArraySlope1.length*countTreesArraySlope2.length*countTreesArraySlope3.length*countTreesArraySlope4.length*countTreesArraySlope5.length;
+
+  return productOfTrees
+;
 };
 
 /* Tests */
